9b7d7270280b071347903c024e8022eeac81967c
Marcel Morales <morales.marcelSF@gmail.com>
2019-02-03 14:42:59 +0100

    updated links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,12 +6,11 @@ const Footer = (props) => (
         <ul className="icons">
             <li><a href="https://twitter.com/blockspaceapp" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
             <li><a href="https://www.facebook.com/BlockspaceApp/" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-            <li><a href="#" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
-            <li><a href="#" className="icon fa-telegram"><span className="label">Telegram</span></a></li>
-            <li><a href="#" className="icon fa-android"><span className="label">Android</span></a></li>
-            <li><a href="#" className="icon fa-apple"><span className="label">App-Store</span></a></li>
+            <li><a href="https://www.linkedin.com/company/blockspace/" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
+            <li><a href="https://t.me/blockspaceapp" className="icon fa-telegram"><span className="label">Telegram</span></a></li>
+            <li><a href="https://play.google.com/store/apps/details?id=ch.blockspace.blockspace" className="icon fa-android"><span className="label">Android</span></a></li>
+            <li><a href="https://itunes.apple.com/gb/app/blockspace-crypto-markets/id1348078159" className="icon fa-apple"><span className="label">App-Store</span></a></li>
         </ul>
-        {/* <p className="copyright">&copy; Gatsby Starter - Dimension. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p> */}
     </footer>
 )
 
